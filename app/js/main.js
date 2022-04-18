42dc8b31c9da35ed428df7466edf35be219084fb
Dmitrij <dmitry.bermas@gmail.com>
2022-04-18 14:32:34 +0300

    prepared for deploying on github pages

--- app/js/main.js
+++
@@ -1,8 +0,0 @@
-const openingBtns = document.querySelectorAll('.team-member__story-btn');
-
-openingBtns.forEach(btn => {
-    btn.addEventListener('click', () => {
-        btn.classList.toggle('change');
-        btn.nextElementSibling.classList.toggle('change');
-    });
-});
